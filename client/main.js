79e287b362a3ee4d873f8ee30dd1896de092e61f
Raymonds <rafaelgame2012@gmail.com>
2023-10-28 16:41:39 -0600

    add a function for resize image to enchance the plugin's perfomance

--- client/main.js
+++ client/main.js
@@ -23,15 +23,49 @@ webcam
     console.log(err);
   });
 
+const resizeBase64Img = (base64, maxWidth = 80, maxHeight = 60) => {
+  return new Promise((resolve) => {
+    let img = new Image();
+    img.src = base64;
+    img.onload = () => {
+      let canvas = document.createElement("canvas");
+      const MAX_WIDTH = maxWidth;
+      const MAX_HEIGHT = maxHeight;
+      let width = img.width;
+      let height = img.height;
+
+      if (width > height) {
+        if (width > MAX_WIDTH) {
+          height *= MAX_WIDTH / width;
+          width = MAX_WIDTH;
+        }
+      } else {
+        if (height > MAX_HEIGHT) {
+          width *= MAX_HEIGHT / height;
+          height = MAX_HEIGHT;
+        }
+      }
+
+      canvas.width = width;
+      canvas.height = height;
+
+      let ctx = canvas.getContext("2d");
+      ctx.drawImage(img, 0, 0, width, height);
+
+      resolve(canvas.toDataURL());
+    };
+  });
+};
+
 btnTakePhoto.addEventListener("click", async () => {
   const picture = webcam.snap();
   photoURI = picture;
+  photoURI = await resizeBase64Img(photoURI);
   btnSendForm.className = `btn btn-lg btn-primary`;
   btnSendForm.disabled = false;
 });
 
 form.addEventListener("formdata", (e) => {
   const formData = e.formData;
-  // Cuando se genera el URI
   formData.append("uri", photoURI);
 });
